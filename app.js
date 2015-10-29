508f8cd5d935330f9cad09e930faa6ac0a544f89
Roberto <fellin.roberto@hotmail.it>
2015-10-29 13:15:33 +0100

    fine esercizio

--- app.js
+++ app.js
@@ -6,13 +6,40 @@
 // hidden: when the card has not been discovered yet
 
 $(document).ready(function(){
+    
+    var index=1;
+    for(var i=0;i<data.length;i++){
+        $(".cards").append(" <li id=\""+data[i]+"\" class=\"done\">"+
+                "<h3>"+data[i]+"</h3>"+
+                "</li> ");
+        console.log(data[i]);
+    }
+    
+    $(".opt-start").click(function(){
+        $(".done").addClass("hidden").removeClass("done"); 
+        
+        $(".hidden").click(function(){
+            
+            var c=parseInt($(this).children().text());
+            
+            $(this).addClass("done").removeClass("hidden"); 
+       
+            if(c!==index){
+                alert("ha perso");
+                location.reload();
+            }
+            else if(index===data.length){
+                alert("ha vinto");
+                location.reload();
+            }
+            index++;
+            
+        });
+        
+    });
 
-  
+
+
 });
-
-
-
-
-
 
 
